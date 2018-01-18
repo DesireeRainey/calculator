ab5dfe143be8fd7bdbbcdbc4c6c9032fff9ff54b
Desiree Rainey <Drainey21.DR@gmail.com>
2018-01-17 23:22:19 -0800

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+	constructor(props){
+    super(props)
+    this.state={
+    	val1: 0,
+    	val2: 0,
+        total: 0
+    }
+    this.add = this.add.bind(this);
+  	this.handleChange1 = this.handleChange1.bind(this);
+  	this.handleChange2 = this.handleChange2.bind(this);
+	}
+	add(e){
+		e.preventDefault(e);
+		let num1 =parseInt(this.state.val1)
+		let num2 =parseInt(this.state.val2)
+		this.setState({total: (num1 + num2)})
+	}
+  	handleChange1(event){
+  		this.setState({val1: event.target.value});
+  	}
+  	handleChange2(event){
+  		this.setState({val2: event.target.value});
+  	}
+
+  	
+	render() {
+		return(
+		<div className="container">
+  			<h1>Add with React!</h1>
+  		<form onSubmit={this.add}>
+  		<div className="add">
+    		<input value={this.state.val1} onChange={this.handleChange1} type="text" />
+    		<span>+</span>
+    		<input value={this.state.val2} onChange={this.handleChange2} type="text" />
+    		<span>=</span>
+    		<button onSubmit={this.state.add}></button>
+    		<h3>The total is: {this.state.total}</h3>
+  		</div>
+  		</form>
+		</div>
+		);
+	}
+}
+
+export default Calculator;
